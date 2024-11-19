26a27d5e9b6759f88ef81a0425e4ecf747bf442c
sootopia <shpark7502@naver.com>
2024-11-19 10:12:22 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './assets/css/index.css';
+import GlobalStyle from './styles/GlobalStyles';
+import './assets/css/fonts.css';
+
+const App = () => {
+  return (
+    <>
+      <GlobalStyle />
+      <div className="py-8 font-black">12345</div>
+    </>
+  );
+};
+
+export default App;
