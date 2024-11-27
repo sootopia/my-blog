44d585a03a25c2b840292f30ca674cd856e71af6
sootopia <shpark7502@naver.com>
2024-11-27 17:34:19 +0900

    feat: 포스팅 카드 컴포넌트 추가

---
+++ src/components/post/FeaturedPostCard.tsx
@@ -0,0 +1,111 @@
+import Post from '../../types/post';
+import styled from 'styled-components';
+
+interface FeaturedPostCardProps {
+  post: Post;
+}
+
+const Card = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const ImageContainer = styled.figure`
+  position: relative;
+  overflow: hidden;
+  width: 41.66%;
+  aspect-ratio: 4 / 3;
+  border-radius: 32px;
+  background-color: #f1f5f9;
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+`;
+
+const InfoContainer = styled.div`
+  width: 50%;
+`;
+
+const Category = styled.h4`
+  font-family:
+    'Gabarito',
+    'Pretendard',
+    system-ui,
+    -apple-system,
+    BlinkMacSystemFont,
+    'Segoe UI',
+    Roboto,
+    Oxygen,
+    Ubuntu,
+    Cantarell,
+    'Open Sans',
+    'Helvetica Neue',
+    sans-serif;
+  font-size: 18px;
+  font-weight: 600;
+  color: #4f46e5;
+  margin-bottom: 16px;
+`;
+
+const Title = styled.h3`
+  display: -webkit-box;
+  font-size: 28px;
+  font-weight: 700;
+  color: #000;
+  line-height: 1.64;
+  max-height: 138px;
+  margin-bottom: 40px;
+  text-overflow: ellipsis;
+  overflow: hidden;
+  -webkit-line-clamp: 3;
+  -webkit-box-orient: vertical;
+`;
+
+const BottomInfo = styled.span`
+  display: block;
+  font-size: 14px;
+  font-weight: 500;
+  color: #94a3b8;
+`;
+
+const HashTags = styled.ul`
+  display: flex;
+  margin-top: 28px;
+  gap: 8px;
+
+  li {
+    display: inline-flex;
+    font-size: 14px;
+    font-weight: 600;
+    color: #64748b;
+    padding: 0 8px;
+    height: 28px;
+    border-radius: 10px;
+    background-color: #e2e8f0;
+    align-items: center;
+  }
+`;
+
+const FeaturedPostCard = ({ post }: FeaturedPostCardProps) => {
+  return (
+    <Card>
+      <ImageContainer style={{ backgroundImage: `url(${post.thumbnail})` }} />
+      <InfoContainer>
+        <Category>{post.category}</Category>
+        <Title>{post.subject}</Title>
+        <BottomInfo>
+          {post.date} · 조회 {post.read}
+        </BottomInfo>
+        <HashTags>
+          {post.hashtags.map(tag => (
+            <li key={tag}>#{tag}</li>
+          ))}
+        </HashTags>
+      </InfoContainer>
+    </Card>
+  );
+};
+
+export default FeaturedPostCard;
